a1fe04b41c92a7e8eb9bc9396093e1ea5ba4bd2f
Max Elander <max.elander@mac.com>
2019-04-01 15:08:30 +0200

    klar, sort of

--- src/App.js
+++ src/App.js
@@ -20,9 +20,31 @@ class App extends Component {
   description:"Beskrivning av den andra testnyheten",
   }]};
   }
+
+  componentDidMount() { 
+    fetch("https://newsapi.org/v2/top-headlines?country=se&apiKey=ae24bb6763034855bf26908e36dcd782")
+      .then(function (response) {
+        
+      // gör något med det som kom tillbaka
+      return response.json()
+    } ).then( jsondata => {
+      //gör något med json-objektet
+      this.setState({ articles: jsondata.articles })
+    }).catch(error =>{
+      this.setState({
+        articles: [{
+          urlToImage: "fejk.jpg",
+          title: "Något gick fel",
+          description: "Du verkar inte vara ansluten till internet..."
+        }]
+      });
+    })
+  }
+
   render() {
     return (
-      <Nyhetslista minaArtiklar={this.state.articles}/>
+      <Nyhetslista
+        minaArtiklar={this.state.articles} />
     );
   }
 }
