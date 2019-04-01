f2a9bd7490e79fc8e38660077e0a867c4423e68c
Max Elander <max.elander@mac.com>
2019-04-01 11:04:41 +0200

    lagt till egna komponenter

---
+++ src/components/Nyhetsartikel.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function Nyhetsartikel() {
+  return (
+    <article>
+      <img src="https://source.unsplash.com/random/200×125/?cat" />
+      <h2>Testnyhet</h2>
+      <p>Beskrivning av testnyheten</p>
+      <a>Läs mer..</a>
+    </article>
+  );
+ }
+
+export default Nyhetsartikel;
